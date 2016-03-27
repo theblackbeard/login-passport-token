5b06bb434326e1f80559f712836d7d0e3f4e0143
Tatiana souza santana <Tatiana souza santana>
2016-03-27 16:01:10 -0300

    Token Login API

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
@@ -21,24 +20,6 @@ UserSchema.pre('save', function (next) {
  var user = this;
   if (!user.isModified('password')) return next();
   bcrypt.genSalt(10, function(err, salt) {
-=======
-var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
-
-var Schema = mongoose.Schema;
-
-UserSchema = new Schema({
-	name: String, 
-    password: String, 
-    admin: Boolean,
-    token: String
-})
-
-UserSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) return next();
-  bcrypt.genSalt(5, function(err, salt) {
->>>>>>> 3066045db66d7e9ff255ba45f217fc49d5b04ffd
     if (err) return next(err);
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) return next(err);
@@ -47,7 +28,6 @@ UserSchema.pre('save', function(next) {
     });
   });
 });
-<<<<<<< HEAD
  
 UserSchema.methods.comparePassword = function (passw, cb) {
     bcrypt.compare(passw, this.password, function (err, isMatch) {
@@ -58,15 +38,4 @@ UserSchema.methods.comparePassword = function (passw, cb) {
     });
 };
  
-=======
-
-
-UserSchema.methods.checkPass = function(password, next) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    if (err) return next(err);
-    next(isMatch);
-  });
-};
-
->>>>>>> 3066045db66d7e9ff255ba45f217fc49d5b04ffd
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict'
-<<<<<<< HEAD
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
@@ -11,39 +10,11 @@ const User        = require('./app/models/user'); // get the mongoose model
 const port        = process.env.PORT || 8080;
 const jwt         = require('jwt-simple');
  
-=======
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
-var jwt    = require('jsonwebtoken'); 
-var config = require('./config'); 
-var User   = require('./app/models/user');
-var apiRoutes = express.Router();     
-var port = process.env.PORT || 8080;
-var swig = require('swig');
-var cookieParser = require('cookie-parser');
-var querystring = require('querystring')
-
-app.use(express.static(__dirname + '/public'));
-app.engine('html', swig.renderFile);
-app.set('views', __dirname + '/app/views');
-app.set('view engine', 'html');
-
-
-
-mongoose.connect(config.database); 
-app.set('superSecret', config.secret); 
-
-app.use(cookieParser())
->>>>>>> 3066045db66d7e9ff255ba45f217fc49d5b04ffd
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-<<<<<<< HEAD
 app.use(passport.initialize());
 
 
@@ -145,123 +116,3 @@ app.use('/api', apiRoutes);
 
 app.listen(port);
 console.log('There will be dragons: http://localhost:' + port);
-=======
-app.get('/', function(req, res) {
-    res.send('Hello! The API is at http://localhost:' + port + '/api');
-});
-
-app.get('/login', function(req, res) {
-    res.render('login')
-});
-
-
-
-apiRoutes.post('/authenticate', function(req, res) {
-
-  // find the user
-  User.findOne({
-    name: req.body.name
-  }, function(err, user) {
-  		
-  		if(err) throw err;
-  		if(!user) res.json({ success: false, message: 'Authentication failed. User not found.' });
-   		else if(user){
-   			user.checkPass(req.body.password, function(isMatch){
-   				if (!isMatch) res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-   				else{
-              //let token = jwt.sign(user, app.get('superSecret'), { expiresIn: '12h' });
-              res.redirect('/api?token=' + user.token)
-   				}
-
-   			})
-   				
-   		}
-
-  });
-});
-
-app.get('/setup', function(req, res) {
-
-  // create a sample user
-  var person = new User({ 
-    name: 'taty', 
-    password: '123',
-    admin: true
-    
-  });
-
-  // save the sample user
-  person.save(function(err, user) {
-    if (err) throw err;
-
-       user.token =  jwt.sign(user, app.get('superSecret'), { expiresIn: '12h' });
-       user.save(function(err){
-           if (err) throw err;
-           res.json({ success: true });
-       })
-      
-    });
-});
-
-
-
-// route middleware to verify a token
-apiRoutes.use(function(req, res, next) {
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (token) {
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-      } else {
-        req.decoded = decoded;    
-        next();
-      }
-    });
-  } else {
-    // if there is no token
-    // return an error
-    /*
-    return res.status(403).send({ 
-        success: false, 
-        message: 'No token provided.' 
-    });*/
-	return res.redirect('/login')
-    
-  }
-});
-
-
-apiRoutes.get('/', function(req, res) {
-  res.render('index', {token: req.query.token})
-});
-
-// route to return all users (GET http://localhost:8080/api/users)
-apiRoutes.get('/users', function(req, res) {
-  User.find({}, function(err, users) {
-    res.json(users);
-  });
-});   
-
-apiRoutes.get('/users/new', function(req, res){
-  console.log("ok: " + req.message.rawHeaders)
-  res.render('new_user.html')
-})
-
-
-
-
-
-
-// apply the routes to our application with the prefix /api
-app.use('/api', apiRoutes);
-
-// API ROUTES -------------------
-// we'll get to these in a second
-
-// =======================
-// start the server ======
-// =======================
-app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
->>>>>>> 3066045db66d7e9ff255ba45f217fc49d5b04ffd
